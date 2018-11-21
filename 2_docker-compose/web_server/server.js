d3ba5ff8cf850dab48ba3b7057d772cda319f4db
Steven Carter <stonemason@gmail.com>
2018-11-21 11:59:57 +0200

    Updated the view service to look a little better

--- 2_docker-compose/web_server/server.js
+++ 2_docker-compose/web_server/server.js
@@ -1,17 +1,18 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://' + process.env.MONGODB + ':27017/machines';
-var http = require('http');
 const express = require('express')
 const app = express()
 app.set('view engine', 'pug')
+app.locals.formatBytes = formatBytes
+app.locals.formatLoadAverage = formatLoadAverage
+app.locals.formatUptime = formatUptime
 
 app.get('/', (req, res) => {
   console.log('Received request for URL: ' + req.url);
-  MongoClient.connect(url, function(err, db) {
+  MongoClient.connect(url, function (err, db) {
     assert.equal(null, err);
-    findMachines(db, function(machines) {
+    findMachines(db, function (machines) {
       res.render('index', { machines: machines })
       db.close();
     });
@@ -20,10 +21,10 @@ app.get('/', (req, res) => {
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'))
 
-var findMachines = function(db, callback) {
+var findMachines = function (db, callback) {
   var cursor = db.collection('machines').find();
   var machines = [];
-  cursor.each(function(err, doc) {
+  cursor.each(function (err, doc) {
     assert.equal(err, null);
     if (doc != null) {
       console.log(JSON.stringify(doc))
@@ -33,3 +34,29 @@ var findMachines = function(db, callback) {
     }
   });
 };
+
+function formatBytes(a, b) {
+  if (0 == a)
+    return "0 Bytes";
+  var c = 1024
+  var d = b || 2
+  var e = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+  var f = Math.floor(Math.log(a) / Math.log(c));
+  return parseFloat((a / Math.pow(c, f)).toFixed(d)) + " " + e[f];
+}
+
+function formatLoadAverage(loadAverage) {
+  return parseFloat(loadAverage[0]).toPrecision(2) + ", " + parseFloat(loadAverage[1]).toPrecision(2) + ", " + parseFloat(loadAverage[2]).toPrecision(2)
+}
+
+function formatUptime(seconds){
+  function pad(s){
+    return (s < 10 ? '0' : '') + s;
+  }
+  var hours = Math.floor(seconds / (60*60));
+  var minutes = Math.floor(seconds % (60*60) / 60);
+  var seconds = Math.floor(seconds % 60);
+
+  return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+}
+
